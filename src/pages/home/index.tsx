f758a5efbab24e8a6b684c85c003fdcee11b06ed
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-02-17 21:09:50 -0300

    feature: Iniciando o layout da tela inicial

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,55 @@
+import { useTheme } from 'styled-components'
+import { ShoppingCart, Package, Timer, Coffee } from 'phosphor-react'
+import banner from '@/assets/coffee-delivery.png'
+import * as Styles from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  const theme = useTheme()
+
+  return (
+    <div>
+      <Styles.IntroContainer>
+        <Styles.InfoContainer>
+          <Styles.TitleContainer>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </Styles.TitleContainer>
+          <Styles.ItemsContainer>
+            <li>
+              <Styles.ItemIcon color={theme['yellow-dark']}>
+                <ShoppingCart weight="fill" size={14} />
+              </Styles.ItemIcon>
+              Compra simples e segura
+            </li>
+            <li>
+              <Styles.ItemIcon color={theme['text']}>
+                <Package weight="fill" size={14} />
+              </Styles.ItemIcon>
+              Embalagem mantém o café intacto
+            </li>
+            <li>
+              <Styles.ItemIcon color={theme['yellow']}>
+                <Timer weight="fill" size={14} />
+              </Styles.ItemIcon>
+              Entrega rápida e rastreada
+            </li>
+            <li>
+              <Styles.ItemIcon color={theme['purple']}>
+                <Coffee weight="fill" size={14} />
+              </Styles.ItemIcon>
+              O café chega fresqueinho até você
+            </li>
+          </Styles.ItemsContainer>
+        </Styles.InfoContainer>
+        <img
+          src={banner}
+          alt="Banner de um café com a logo da Coffee Delivery"
+          draggable="false"
+        />
+      </Styles.IntroContainer>
+    </div>
+  )
 }
