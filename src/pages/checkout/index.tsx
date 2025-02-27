86cd1f8de9894ad2cc3caf887c3931512fd87a9c
Tiago Lopes <tiagoltavares2002@gmail.com>
2025-02-26 22:48:13 -0300

    feature: terminando a estilização dos formulários de endereço e pagamento

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,10 +1,86 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money
+} from 'phosphor-react'
 import * as Styles from './styles'
+import { useTheme } from 'styled-components'
 
 export function Checkout() {
+  const theme = useTheme()
+
   return (
     <Styles.CheckoutContainer>
       <Styles.CheckoutFormContainer>
         <h2>Complete seu pedido</h2>
+
+        <Styles.FormContainer>
+          <header>
+            <span>
+              <MapPinLine size={22} color={theme['yellow-dark']} />
+            </span>
+            <div>
+              <h3>Endereço de Entrega</h3>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </header>
+
+          <Styles.AddressForm>
+            <Styles.InputGroup>
+              <Styles.FormInput placeholder="CEP" width="30%" />
+              <Styles.FormInput placeholder="Rua" />
+            </Styles.InputGroup>
+            <Styles.InputGroup>
+              <Styles.FormInput placeholder="Número" width="30%" />
+              <Styles.FormInput placeholder="Complemento" />
+            </Styles.InputGroup>
+            <Styles.InputGroup>
+              <Styles.FormInput placeholder="Bairro" />
+              <Styles.FormInput placeholder="Cidade" />
+              <Styles.FormInput placeholder="UF" width="20%" />
+            </Styles.InputGroup>
+          </Styles.AddressForm>
+        </Styles.FormContainer>
+
+        <Styles.FormContainer>
+          <header>
+            <span>
+              <CurrencyDollar size={22} color={theme['purple']} />
+            </span>
+            <div>
+              <h3>Pagamento</h3>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </header>
+
+          <Styles.PaymentOptionContainer>
+            <Styles.PaymentOption>
+              <input type="radio" name="payment" id="credit-card" />
+              <label htmlFor="credit-card">
+                <CreditCard size={16} color={theme['purple']} />
+                Cartão de crédito
+              </label>
+            </Styles.PaymentOption>
+            <Styles.PaymentOption>
+              <input type="radio" name="payment" id="debit-card" />
+              <label htmlFor="debit-card">
+                <Bank size={16} color={theme['purple']} />
+                Cartão de debito
+              </label>
+            </Styles.PaymentOption>
+            <Styles.PaymentOption>
+              <input type="radio" name="payment" id="cash" />
+              <label htmlFor="cash">
+                <Money size={16} color={theme['purple']} />
+                Dinheiro
+              </label>
+            </Styles.PaymentOption>
+          </Styles.PaymentOptionContainer>
+        </Styles.FormContainer>
       </Styles.CheckoutFormContainer>
 
       <Styles.OrderContainer>
--- src/pages/checkout/styles.ts
+++ src/pages/checkout/styles.ts
@@ -18,6 +18,61 @@ export const CheckoutFormContainer = styled.div`
   }
 `
 
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  padding: 40px;
+  background-color: ${({ theme }) => theme.card};
+  border-radius: 6px;
+
+  header {
+    display: flex;
+    gap: 8px;
+
+    div {
+      display: flex;
+      flex-direction: column;
+
+      h3 {
+        font-family: 'Baloo 2', sans-serif;
+        font-size: 1.125rem;
+        font-weight: 500;
+        color: ${({ theme }) => theme.subtitle};
+      }
+
+      p {
+        font-size: 0.875rem;
+        color: ${({ theme }) => theme.text};
+      }
+    }
+  }
+`
+
+export const AddressForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+`
+
+export const InputGroup = styled.div`
+  display: flex;
+  gap: 0.75rem;
+`
+
+export const FormInput = styled.input<{ width?: string }>`
+  padding: 0.75rem;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.button};
+  background-color: ${({ theme }) => theme.input};
+  &:focus,
+  &:active {
+    outline: 1px solid ${({ theme }) => theme['yellow-dark']};
+  }
+
+  width: ${(props) => props.width || '100%'};
+`
+
 export const OrderContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -27,5 +82,36 @@ export const OrderContainer = styled.div`
     font-family: 'Baloo 2', sans-serif;
     font-size: 1.125rem;
     color: ${({ theme }) => theme.subtitle};
+  }
+`
+
+export const PaymentOptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`
+
+export const PaymentOption = styled.div`
+  flex: 1;
+
+  input[type='radio'] {
+    display: none;
+  }
+
+  label {
+    background-color: ${({ theme }) => theme.button};
+    border-radius: 6px;
+    border: none;
+    cursor: pointer;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+  }
+
+  input[type='radio']:checked + label {
+    background-color: ${({ theme }) => theme['purple-light']};
+    outline: 1px solid ${({ theme }) => theme.purple};
   }
 `
